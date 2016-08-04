f79a6a195c4afb581b75ba951861b574abf5f6d4
Aozaki <rel124c41p@gmail.com>
2016-08-04 14:32:45 +0500

    GP074

--- fgo/item/gameplay_profiles/js/nav.js
+++ fgo/item/gameplay_profiles/js/nav.js
@@ -4,9 +4,9 @@ function getSvt(ID)
 {
 	var src = path + ID + ".html";
 	$(".svt").load(src, function(html){
-		$(".svtInfo").empty().append(html.find(".svtInfo"));
-		$(".svtGameplay").empty().append(html.find(".svtGameplay"));
-		$(".svtTips").empty().append(html.find(".svtTips"));
+		$(".svtInfo").empty().append($(".svtInfo"), html);
+		$(".svtGameplay").empty().append($(".svtGameplay"), html);
+		$(".svtTips").empty().append($(".svtTips"), html);
 	});
 }
 
