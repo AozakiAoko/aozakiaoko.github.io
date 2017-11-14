b51ad9bb89b9883c780d64763f2a7af1c60d8b69
aozaki-desu <rel214c41p@gmail.com>
2017-11-14 13:07:35 +0500

    tierlist update 12
    test js

--- fgo/item/tierlist/tierlist.js
+++ fgo/item/tierlist/tierlist.js
@@ -1,1 +1,6 @@
 document.getElementById('youridhere').scrollIntoView();
+
+$.(".tierlist tr td div").on('click', function(){
+	$(".SvtImg").empty().append( $(this).html() );
+	$(".Explanation").empty().append($(".tips #" + $(this).attr('id')).html();
+});
