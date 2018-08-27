dbec1327f0194927c417bc7eb48dd2df2c58fddf
aozaki-desu <rel214c41p@gmail.com>
2018-08-27 15:04:58 +0500

    db002

--- fgo/item/database/js/index.js
+++ fgo/item/database/js/index.js
@@ -2,5 +2,18 @@ function printServant(svtData){
   $("#svt").empty().append(svtData.Name);
 };
 
-var testsvtdata = require('./data/0.json'); //(with path)
-printServant(testsvtdata);
+ function loadJSON(path, callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+var testsvtdata = loadJSON('./data/0.json', printServant(testsvtdata));
