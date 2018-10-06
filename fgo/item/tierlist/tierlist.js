66c3c0290bf5f4e86d87e5184818bff95536bdd7
aozaki-desu <rel214c41p@gmail.com>
2018-10-06 13:31:56 +0500

    061018

--- fgo/item/tierlist/tierlist.js
+++ fgo/item/tierlist/tierlist.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
 	$(".tierlist tr td div").on('click', function(){
 		$(".SvtImg").empty().append( '<div class="' + $(this).attr("class") +'"></div>' );
-		$(".Explanation").empty().append($(".tips ." + $(this).attr("class").split(' ')[1]).html());
+		//$(".Explanation").empty().append($(".tips ." + $(this).attr("class").split(' ')[1]).html());
+
+        $(".Explanation td .spancontent").empty().append( 
+            $( $(this).attr("class").split(' ')[1]  + " .spancontent").html()
+        );
 	});
 
-	$(".categories_all td").on('click', function(){
+	$(".Explanation table td").on('click', function(){
 		$(".parameters_display_explanation").empty()
 		.append($('.parameters_explanation > .' + $(this).attr("class").split(' ')[1]).html());
 	});	
